8c315a9eaac6119fc2a0217cb4e83ff73e618573
upadhharshit <upadhharshit987@gmail.com>
2023-10-16 09:58:00 +0530

    assigmnt

--- src/components/useToggle.js
+++ src/components/useToggle.js
@@ -1,7 +1,22 @@
-import React,{useCallback,useState} from 'react'
+import React, { useCallback, useState } from 'react';
+
+const useToggle = (initialState = false) => {
+  const [state, setState] = useState(initialState);
+
+  const toggle = useCallback(() => {
+    setState((prevState) => !prevState);
+  }, []);
+
+  return [state, toggle];
+};
+
+export default useToggle;
+
+
+/*import React,{useCallback,useState} from 'react'
 
 const useToggle = () => {
    
   
   }
-  export default useToggle;
+  export default useToggle;*/
